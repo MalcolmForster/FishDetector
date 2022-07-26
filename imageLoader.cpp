52fdbf3b1bb05ff8d062fbd93acea52e443ff9e7
Malcolm <forster.malcolm@gmail.com>
2022-07-26 22:41:06 +1200

    Sorting out compile errors, link probs remain

--- imageLoader.cpp
+++ imageLoader.cpp
@@ -1,7 +1,8 @@
 #include <opencv2/core.hpp>
+#include <opencv2/core/cuda.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
-
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace cv;
